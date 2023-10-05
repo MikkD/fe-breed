a0bfe255f5a04d413c190d6d0a694720c24f0d40
Mikita <ndosyukov@gmail.com>
2023-10-05 13:21:16 -0700

    teaching-added-controlled-form

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AnimalsPage from './pages/AnimalsPage';
+
+function App() {
+    return (
+        <div className='App'>
+            <AnimalsPage />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/utils.js
@@ -0,0 +1,5 @@
+export const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile'];
+export const BASE_URL = 'http://pets-v2.dev-apis.com';
+
+// const URL = `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`;
+// const BREEDS_URL = `http://pets-v2.dev-apis.com/breeds?animal=${animal}`;
